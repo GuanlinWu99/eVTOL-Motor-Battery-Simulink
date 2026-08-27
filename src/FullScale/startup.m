ed2db4eca1d0467b236d0622e910914d5cccce3a
wgl <m18342291462@gmail.com>
2026-08-27 19:59:43 -0400

    chore: drop accelerator mode, add a mac short-run driver

--- src/FullScale/startup.m
+++ src/FullScale/startup.m
@@ -203,7 +203,6 @@ if uavParams.Temperature == 20
 end
 
 %% Run Simulink
-set_param(mdl,'SimulationMode','accelerator');   % rapid: compile to native code (first run builds, then fast)
 outTuned = sim(mdl);
 filename = sprintf('P%d_MTOW_%d_Wind_%d_Temp_%d.mat', Profile, eVTOL_MTOW, Wind_Speed, target_temperature);
 save(filename, 'outTuned', 'TransitionMission', 'uavParams', 'controlParams', 'const');
